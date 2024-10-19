0028736212dc48c0b7bcfe0d98e7c4f99a67ab43
Wenyu Huang <huangwenyuu@outlook.com>
2024-10-19 00:15:39 -0400

    Add desc crate and split.rs
    Add desc crate for split and packed descriptor.
    The Descriptor struct in mod.rs represents memory layout.
    
    Signed-off-by: Wenyu Huang <huangwenyuu@outlook.com>

---
+++ virtio-queue/src/desc/mod.rs
@@ -0,0 +1,34 @@
+//! Descriptor types for virtio queue.
+
+use vm_memory::{ByteValued, Le16, Le32, Le64};
+
+pub mod split;
+
+/// A virtio descriptor's layout constraints with C representation.
+/// This is a unified representation of the memory layout order
+/// for packed descriptors and split descriptors.
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Default)]
+pub struct Descriptor(Le64, Le32, Le16, Le16);
+
+// SAFETY: This is safe because `Descriptor` contains only wrappers over POD types and
+// all accesses through safe `vm-memory` API will validate any garbage that could be
+// included in there.
+unsafe impl ByteValued for Descriptor {}
+
+impl From<split::Descriptor> for Descriptor {
+    fn from(desc: split::Descriptor) -> Self {
+        Descriptor(
+            Le64::from(desc.addr().0),
+            Le32::from(desc.len()),
+            Le16::from(desc.flags()),
+            Le16::from(desc.next()),
+        )
+    }
+}
+
+impl From<Descriptor> for split::Descriptor {
+    fn from(desc: Descriptor) -> split::Descriptor {
+        split::Descriptor::new(desc.0.into(), desc.1.into(), desc.2.into(), desc.3.into())
+    }
+}
